7c4451adcf9ec4d0c6c81433f66f3c6d8ac06530
raiyan <raiyan.m1201@gmail.com>
2024-07-04 15:45:50 -0400

    mongo shell and catalogue shell added

---
+++ mongo.sh
@@ -0,0 +1,6 @@
+cp mongo.repo /etc/yum.repos.d/mongo.repo
+dnf install mongodb-org -y
+systemctl enable mongod
+systemctl start mongod
+sed -i 's/127.0.0.1/0.0.0.0/' /etc/mongod.conf
+systemctl restart mongod
