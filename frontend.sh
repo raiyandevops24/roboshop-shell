f72f33bd3693241651c795059b8404409c29ec8b
raiyan <raiyan.m1201@gmail.com>
2024-07-04 15:17:13 -0400

    frontend shell script update

--- frontend.sh
+++ frontend.sh
@@ -1,10 +1,10 @@
-cp nginx.conf /etc/nginx/nginx.conf
 dnf module disable nginx -y
 dnf module enable nginx:1.24 -y
 dnf install nginx -y
 systemctl enable nginx
 systemctl start nginx
 rm -rf /usr/share/nginx/html/*
+cp nginx.conf /etc/nginx/nginx.conf
 curl -o /tmp/frontend.zip https://roboshop-artifacts.s3.amazonaws.com/frontend-v3.zip
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
