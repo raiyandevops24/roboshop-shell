a288d1e036ac3db310fedcafe6ccb58c12af4253
raiyan <raiyan.m1201@gmail.com>
2024-07-09 00:00:13 -0400

    nodejs function added

--- user.sh
+++ user.sh
@@ -1,7 +1,9 @@
+source common.sh
+
 cp user.service /etc/systemd/system/user.service
-dnf module disable nodejs -y
-dnf module enable nodejs:20 -y
-dnf install nodejs -y
+
+NODEJS
+
 useradd roboshop
 mkdir /app
 curl -L -o /tmp/user.zip https://roboshop-artifacts.s3.amazonaws.com/user-v3.zip
