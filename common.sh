5d71ea5db08f5a28aa4706c2ff15daa8b9fe570e
raiyan <raiyan.m1201@gmail.com>
2024-07-10 06:40:56 -0400

    catalogue updated

--- common.sh
+++ common.sh
@@ -11,57 +11,113 @@ PRINT(){
 NODEJS(){
   PRINT Disable NodeJS Default Version
   dnf module disable nodejs -y &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Enable NodeJS 20 Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Install NodeJS
   dnf install nodejs -y &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Copy Service File
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Copy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Adding Application User
   useradd roboshop &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Removing Old Content
   rm -rf /app &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Create App Directory
   mkdir /app &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   cd /app
 
   PRINT Extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   cd /app
 
   PRINT Download NodeJS Dependencies
   npm install &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 
   PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
   systemctl enable ${component} &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
   systemctl restart ${component} &>>$LOG_FILE
-  echo $?
+  if [ $? -eq 0]; then
+    echo SUCCESS
+  else
+    echo FAILURE
+  fi
 }
