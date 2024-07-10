cb8928ba12ed30dfe337f78546e554391e58cd85
raiyan <raiyan.m1201@gmail.com>
2024-07-10 06:13:25 -0400

    catalogue updated

--- catalogue.sh
+++ catalogue.sh
@@ -5,7 +5,7 @@ component=catalogue
 NODEJS
 
 echo Install MongoDB Client
-dnf install mongodb-mongosh -y &>$LOG_FILE
+dnf install mongodb-mongosh -y &>>$LOG_FILE
 
 echo Load Master Data
-mongosh --host mongo.dev.raiyan-m.online </app/db/master-data.js &>$LOG_FILE
+mongosh --host mongo.dev.raiyan-m.online </app/db/master-data.js &>>$LOG_FILE
