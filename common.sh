0341821f5289f1b3147948f931337c7156c96ae2
raiyan <raiyan.m1201@gmail.com>
2024-07-10 07:01:53 -0400

    catalogue updated

--- common.sh
+++ common.sh
@@ -8,116 +8,70 @@ PRINT(){
 
 }
 
+STAT(){
+  if [ $1 -eq 0 ]; then
+      echo SUCCESS
+    else
+      echo FAILURE
+    fi
+}
+
 NODEJS(){
   PRINT Disable NodeJS Default Version
   dnf module disable nodejs -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Enable NodeJS 20 Module
   dnf module enable nodejs:20 -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Install NodeJS
   dnf install nodejs -y &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Copy Service File
   cp ${component}.service /etc/systemd/system/${component}.service &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Copy MongoDB repo file
   cp mongo.repo /etc/yum.repos.d/mongo.repo &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Adding Application User
   useradd roboshop &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Removing Old Content
   rm -rf /app &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Create App Directory
   mkdir /app &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Download App Content
   curl -o /tmp/${component}.zip https://roboshop-artifacts.s3.amazonaws.com/${component}-v3.zip &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   cd /app
 
   PRINT Extract App Content
   unzip /tmp/${component}.zip &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   cd /app
 
   PRINT Download NodeJS Dependencies
   npm install &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 
   PRINT Start Service
   systemctl daemon-reload &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
+
   systemctl enable ${component} &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
+
   systemctl restart ${component} &>>$LOG_FILE
-  if [ $? -eq 0 ]; then
-    echo SUCCESS
-  else
-    echo FAILURE
-  fi
+  STAT $?
 }
